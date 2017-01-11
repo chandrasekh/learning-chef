d9288aeb7e191f4336e723fd6e6b4add11963221
chandrasekh <chandrasekhar.c@ncodeit.com>
2017-01-11 12:18:36 +0530

    Import application_ruby version 3.0.2

--- cookbooks/application_ruby/resources/passenger_apache2.rb
+++ cookbooks/application_ruby/resources/passenger_apache2.rb
@@ -1,11 +1,14 @@
 #
-# Copyright 2015, Noah Kantrowitz
+# Cookbook Name:: application_ruby
+# Resources:: passenger
+#
+# Copyright 2012, ZephirWorks
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,6 +17,9 @@
 # limitations under the License.
 #
 
-module PoiseApplicationRuby
-  VERSION = '4.0.1'
-end
+include ApplicationCookbook::ResourceBase
+
+attribute :server_aliases, :kind_of => [Array, NilClass], :default => nil
+# Actually defaults to "#{application.name}.conf.erb", but nil means it wasn't set by the user
+attribute :webapp_template, :kind_of => [String, NilClass], :default => nil
+attribute :params, :kind_of => [Hash, NilClass], :default => nil
